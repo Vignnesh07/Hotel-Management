a347266037987ba591f1f066d9dc6e0ddde1f4ca
Vignnesh <ravin.vignnesh@gmail.com>
2022-08-24 21:17:35 +0800

    Added firebase package and initialised app

---
+++ firebase/firebase-config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLBTg8R7kqHb3XdorP3bgxM7NyLaLe4Yk",
+  authDomain: "hotel-booking-9b0b2.firebaseapp.com",
+  projectId: "hotel-booking-9b0b2",
+  storageBucket: "hotel-booking-9b0b2.appspot.com",
+  messagingSenderId: "459815060485",
+  appId: "1:459815060485:web:25e2f94f78afcbcc1e086c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const authentication = getAuth(app);
