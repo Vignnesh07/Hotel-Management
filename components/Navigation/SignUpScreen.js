651b335af34b05cc2f07df6007406a1133124950
Jevonzz <109259972+Jevonzz@users.noreply.github.com>
2022-08-24 15:04:26 +0800

    Updated SignUpScreen
    Added form validation

--- components/Navigation/SignUpScreen.js
+++ components/Navigation/SignUpScreen.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 import {Text, View, StyleSheet, Modal, Dimensions, TouchableOpacity, StatusBar, Image, useWindowDimensions, TextBase} from "react-native";
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -6,7 +6,63 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import Colors from '../const/color'
 import { TextInput } from "react-native-gesture-handler";
 
-const SignUpScreen = ({navigation,value,setValue}) => {
+const isValidObjField = (obj) =>{
+    return Object.values(obj).every(value=> value.trim())
+}
+
+const updateError = (error,stateUpdater) =>{
+    stateUpdater(error);
+    setTimeout(() => {
+        stateUpdater('')
+    }, 2500);
+}
+
+const isValidEmail = (value) =>{
+    const regx = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    return regx.test(value)
+}
+
+const SignUpScreen = ({navigation,onPress}) => {
+    const [userInfo, setUserInfo] = useState({
+        username:'',
+        phone_num: '',
+        email:'',
+        password:'',
+        confirm_password: '',
+    })
+
+    const [error, setError] = useState('')
+
+    const{username,phone_num,email,password,confirm_password} = userInfo
+
+    const handleOnChangeText = (value,fieldName)=> {
+        setUserInfo({...userInfo,[fieldName]:value})
+    };
+
+    const isValidForm = () =>{
+        if(!isValidObjField(userInfo)) return updateError('Required to fill all fields!' , setError)
+
+        if(username.length < 5 ) return updateError('Minimum 5 Characters required for Username!' , setError)
+
+        if(username.length > 9 ) return updateError('Maximum 9 Characters required for Username!' , setError)
+
+        if(phone_num.length < 10 || phone_num.length > 12) return updateError('Invalid Phone Number!' , setError)
+
+        if(!isValidEmail(email)) return updateError('Invalid Email!' , setError)
+        
+        if(password.length < 7 ) return updateError('Minimum 8 Characters required for Password!' , setError)
+
+        if(password !== confirm_password) return updateError('Password does not match!' , setError)
+    
+    return true;
+
+    };
+    const submitForm = ()=>{
+        if(isValidForm()){
+            console.log(userInfo)
+        }
+    };
+
     return(
         <View style={styles.container}>
             <StatusBar backgroundColor={Colors.white} barStyle="dark-content" />
@@ -29,30 +85,50 @@ const SignUpScreen = ({navigation,value,setValue}) => {
                         fontWeight: 'bold'}}>Let's Get You Registered !
                     </Text>
             </View>
+
+            {error ? ( 
+            <Text style = {{color : 'red' ,fontSize : 18, textAlign: 'center'}}>
+                {error}
+            </Text>
+            ):null }
             <View style={styles.inputContainer} >
-                <TextInput  placeholder="Username" value={value} onChangeText={setValue}  style={styles.input} />
+                <TextInput  placeholder="Username"  
+                value={username}
+                onChangeText = {(value) => handleOnChangeText(value,'username')}
+                style={styles.input} />
             </View>
 
             <View style={styles.inputContainer} >
-                <TextInput  placeholder="Phone Number"  keyboardType={'phone-pad'} value={value} onChangeText={setValue}  style={styles.input} />
+                <TextInput  placeholder="Phone Number"
+                value={phone_num}  
+                onChangeText = {(value) => handleOnChangeText(value,'phone_num')}
+                keyboardType={'phone-pad'} 
+                style={styles.input} />
             </View>
 
             <View style={styles.inputContainer} >
-                <TextInput  placeholder="Email Address" keyboardType={'email-address'} value={value} onChangeText={setValue}  style={styles.input} />
+                <TextInput  placeholder="Email Address" 
+                value={email}
+                onChangeText = {(value) => handleOnChangeText(value,'email')}
+                keyboardType={'email-address'} 
+                autoCapitalize = 'none'
+                style={styles.input} />
             </View>
             <View style={styles.inputContainer} >
                 <TextInput  
                     placeholder="Password" 
-                    value={value} 
-                    onChangeText={setValue} 
+                    value={password}
+                    onChangeText = {(value) => handleOnChangeText(value,'password')}
+                    autoCapitalize = 'none'
                     secureTextEntry={true}
                     style={styles.input} />
             </View>
             <View style={styles.inputContainer} >
                 <TextInput  
                     placeholder="Confirm Password" 
-                    value={value} 
-                    onChangeText={setValue} 
+                    value = {confirm_password}
+                    onChangeText = {(value) => handleOnChangeText(value,'confirm_password')}
+                    autoCapitalize = 'none'
                     secureTextEntry={true}
                     style={styles.input} />
             </View>
@@ -68,7 +144,7 @@ const SignUpScreen = ({navigation,value,setValue}) => {
             </View>
 
             <View style={styles.signUpButton}>
-                <TouchableOpacity onPress={() => navigation.navigate('ProfileLogedIn')}>
+                <TouchableOpacity onPress={submitForm}>
                     <View style={styles.buttonContainer}>
                         <Text style={{color: Colors.white, fontSize: 20, fontWeight: 'bold'}}>Sign Up</Text>
                     </View>
